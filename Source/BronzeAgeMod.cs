fbf17bd936f0ac4e5346512a3bfc8451f664c7e2
Andy Brenneke <andy.brenneke@gmail.com>
2020-08-10 01:59:48 -0700

    RimWorld Cookiecutter Init

---
+++ Source/BronzeAgeMod.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System;
+using HugsLib;
+using JetBrains.Annotations;
+using Verse;
+
+namespace ABrenneke.BronzeAge
+{
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature, ImplicitUseTargetFlags.WithMembers)]
+    public class BronzeAgeMod : ModBase
+    {
+        public override string LogIdentifier => "abrenneke.bronze-age";
+
+        public BronzeAgeMod()
+        {
+            // HugsLib Documentation: https://github.com/UnlimitedHugs/RimworldHugsLib/wiki
+        }
+    }
+}
