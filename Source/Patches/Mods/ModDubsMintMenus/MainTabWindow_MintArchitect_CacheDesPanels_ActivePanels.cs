a2e1b63488f9714c1dfa1e9c457e2dbc028ffed1
Andy Brenneke <andy.brenneke@gmail.com>
2020-09-25 23:53:20 -0700

    Sort architect alphabetically and remove tabs with nothing in them until researched.

---
+++ Source/Patches/Mods/ModDubsMintMenus/MainTabWindow_MintArchitect_CacheDesPanels_ActivePanels.cs
@@ -0,0 +1,65 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.Mods.ModDubsMintMenus
+{
+    [ModPatch("dubwise.dubsmintmenus", "DubsMintMenus.MainTabWindow_MintArchitect", "get_CacheDesPanels")]
+    public static class MainTabWindow_MintArchitect_CacheDesPanels_ActivePanels
+    {
+        private static readonly HashSet<string> IgnoreDesignators = new HashSet<string>
+        {
+            "Designator_RemovePipe",
+            "Designator_CleanOil" ,
+            "Designator_RemovePipeline",
+            "Designator_RemoveSkylights"
+        };
+
+        private static readonly List<ArchitectCategoryTab> CachedList = new List<ArchitectCategoryTab>();
+
+        private static List<ArchitectCategoryTab>? desCategories;
+
+        // ReSharper disable once RedundantAssignment
+        public static bool Prefix(ref List<ArchitectCategoryTab> __result)
+        {
+            if (desCategories == null)
+            {
+                var allCategories = DefDatabase<DesignationCategoryDef>.AllDefsListForReading;
+                var alphaSortedCategories = allCategories.OrderBy(x => (string)x.LabelCap, StringComparer.CurrentCulture);
+
+                desCategories = alphaSortedCategories.Select(cat => new ArchitectCategoryTab(cat)).ToList();
+            }
+
+            // TODO cache more, could be faster. Don't notice slowdown right now, though.
+
+            CachedList.Clear();
+            foreach (var cat in desCategories)
+            {
+                bool allow = false;
+                foreach (var designator in cat.def.ResolvedAllowedDesignators)
+                {
+                    if (designator.Visible && !(
+                        designator is Designator_Cancel
+                        || designator is Designator_Deconstruct
+                        || designator is Designator_Uninstall
+                        || IgnoreDesignators.Contains(designator.GetType().Name) ))
+                    {
+                        allow = true;
+                        break;
+                    }
+                }
+
+                if (allow)
+                {
+                    CachedList.Add(cat);
+                }
+            }
+
+            __result = CachedList;
+            return false;
+        }
+    }
+}
