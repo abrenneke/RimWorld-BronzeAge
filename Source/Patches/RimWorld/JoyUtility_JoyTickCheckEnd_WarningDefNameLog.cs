dc00d5a44622eec70ff25b2c4729a29215489b68
Andy Brenneke <andy.brenneke@gmail.com>
2020-09-25 23:53:32 -0700

    Add log for strange warning

---
+++ Source/Patches/RimWorld/JoyUtility_JoyTickCheckEnd_WarningDefNameLog.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.RimWorld
+{
+    [HarmonyPatch(typeof(JoyUtility), nameof(JoyUtility.JoyTickCheckEnd))]
+    public static class JoyUtility_JoyTickCheckEnd_WarningDefNameLog
+    {
+        public static void Prefix(Pawn pawn)
+        {
+            if (pawn.CurJob.def.joyKind == null)
+            {
+                Log.Warning($"JoyUtility.JoyTickCheckEnd - Pawn {pawn.ToStringSafe()} has job {pawn.CurJob.def.ToStringSafe()} without joy kind.");
+            }
+        }
+    }
+}
