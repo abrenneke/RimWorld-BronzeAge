99ad1c5c163fcece3e24039c17f0bead6a85e393
Andy Brenneke <andy.brenneke@gmail.com>
2020-09-25 18:39:44 -0700

    Fix #81 - add null check to rimocracy
    Fix RemovePositiveBedroomThoughts error, hospitality + remember beds?

---
+++ Source/Patches/Mods/Rimocracy/StatPart_Governance_Multiplier_ExceptionFix.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.Mods.Rimocracy
+{
+    [ModPatch("garwel.rimocracy", "Rimocracy.StatPart_Governance", "Multiplier")]
+    public static class StatPart_Governance_Multiplier_ExceptionFix
+    {
+        // Mod does not check if req.Thing.Faction is null
+        public static bool Prefix(StatRequest req, ref float __result)
+        {
+            var isError = req.HasThing && req.Thing is Building && req.Thing.Faction == null;
+            if (isError)
+                __result = 1;
+
+            return !isError;
+        }
+    }
+}
---
+++ Source/Patches/RimWorld/ThoughtUtility_RemovePositiveBedroomThoughts_ErrorFix.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.RimWorld
+{
+    [HarmonyPatch(typeof(ThoughtUtility), nameof(ThoughtUtility.RemovePositiveBedroomThoughts))]
+    public static class ThoughtUtility_RemovePositiveBedroomThoughts_ErrorFix
+    {
+        // pawn.needs null while loading, just ignore that, should be fine
+        public static bool Prefix(Pawn pawn)
+        {
+            return pawn?.needs?.mood != null;
+        }
+    }
+}
