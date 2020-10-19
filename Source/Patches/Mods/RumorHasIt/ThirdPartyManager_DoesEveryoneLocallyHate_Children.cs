be94e3fbc8a809e87c7c900c67d70e0570c7adab
Andy Brenneke <andy.brenneke@gmail.com>
2020-10-19 05:19:43 -0700

    Changes below:
    Add mod configuration files
    Add patchwork cloth
    Fix universal fermenter being basic work??
    Remove smithy and move its recipes to blacksmithing
    Research bench not require steel
    Ship chunks drop slag
    Bloomery inefficiently smelts steel slag
    Fix some bandages having risk of stab
    Rename smithing
    Move dying vat into medieval clothing
    Show unlocked medieval clothing
    Children cannot defect or be involved in cliques

---
+++ Source/Patches/Mods/RumorHasIt/ThirdPartyManager_DoesEveryoneLocallyHate_Children.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.Mods.RumorHasIt
+{
+    [ModPatch("mlie.rfrumorhasit", "Rumor_Code.ThirdPartyManager", "DoesEveryoneLocallyHate")]
+    public static class ThirdPartyManager_DoesEveryoneLocallyHate_Children
+    {
+        public static void Postfix(Pawn p, ref bool __result)
+        {
+            // TODO configurable age
+            __result = __result && p.ageTracker.AgeBiologicalYears < 15;
+        }
+    }
+}
---
+++ Source/Patches/Mods/RumorHasIt/ThirdPartyManager_FindCliques_Children.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using HarmonyLib;
+using Verse;
+
+namespace ABrenneke.BronzeAge.Patches.Mods.RumorHasIt
+{
+    [ModPatch("mlie.rfrumorhasit", "Rumor_Code.ThirdPartyManager", "FindCliques")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static class ThirdPartyManager_FindCliques_Children
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var get_FreeColonistsSpawned = AccessTools.PropertyGetter(typeof(MapPawns), nameof(MapPawns.FreeColonistsSpawned));
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Calls(get_FreeColonistsSpawned))
+                {
+                    yield return CodeInstruction.Call(typeof(ThirdPartyManager_FindCliques_Children), nameof(FreeNonSlaveColonistsSpawned));
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+
+        public static IEnumerable<Pawn> FreeNonSlaveColonistsSpawned(MapPawns instance)
+        {
+            // TODO configurable age
+            return instance.FreeColonistsSpawned.Where(p => p.ageTracker?.AgeBiologicalYears >= 15);
+        }
+    }
+}
